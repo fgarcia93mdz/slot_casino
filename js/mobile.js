2315d7384cbd3408d3868067019457e781e8102c
Franco <frangarcia93@icloud.com>
2023-11-18 10:18:33 -0300

    corrección en diferenciador de mobile

--- js/mobile.js
+++ js/mobile.js
@@ -1,8 +1,10 @@
-var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-var highlightElement = document.getElementById('Highlight');
+document.addEventListener('DOMContentLoaded', function () {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  var highlightElement = document.getElementById('Highlight');
 
   if (/android/i.test(userAgent)) {
     highlightElement.classList.add("android");
   } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
     highlightElement.classList.add("ios");
   }
+});
