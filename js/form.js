060b1a06920dee144be4de5fb7d4d1d37971fb56
Franco <frangarcia93@icloud.com>
2023-12-04 19:22:49 -0300

    Correccion js

--- js/form.js
+++ js/form.js
@@ -4,9 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
     form.addEventListener('submit', function (event) {
       event.preventDefault();
 
-      // Asegúrate de que el formulario todavía está en el DOM
       if (document.body.contains(event.target)) {
-        // Tu código para manejar la presentación del formulario aquí
+
       } else {
         console.error('El formulario ha sido eliminado del DOM');
       }
