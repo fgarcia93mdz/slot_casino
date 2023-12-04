c2085d030f27dc4814f0518d36b6996d0834ef76
Franco <frangarcia93@icloud.com>
2023-12-04 18:24:30 -0300

    Correcciones y mejoras en listado de gastronomia y eventos

--- js/gastronomy.js
+++ js/gastronomy.js
@@ -19,3 +19,33 @@ function closeAlert() {
   alertBox.style.visibility = 'hidden';
   alertBox.style.opacity = '0';
 }
+
+window.onload = function () {
+  var priceElements = document.querySelectorAll('.badge');
+  var comidasAlPasoElements = document.querySelectorAll('#takeAndWay .list-group-item');
+  var platosElaboradosElements = document.querySelectorAll('#elaborateDish .list-group-item');
+  var cervezasElements = document.querySelectorAll('#beer .list-group-item');
+  var aperitivosElements = document.querySelectorAll('#appetizer .list-group-item');
+
+  var comidasAlPaso = ['Bruschetta', 'Calamares Fritos', 'Pan con Ajo', 'Aros de Cebolla', 'Papas con Queso', 'Tacos de Pescado', 'Hot Dogs'];
+  var platosElaborados = ['Espaguetis a la Boloñesa', 'Pizza Margherita', 'Pollo Parmesano', 'Lasaña', 'Salmón a la Parrilla', 'Filete Wellington', 'Paella Vegetariana'];
+  var cervezas = ['Corona', 'Heineken', 'Budweiser', 'Guinness', 'IPA', 'Stella Artois', 'Blue Moon', 'Modelo'];
+  var aperitivos = ['Nachos', 'Alitas', 'Palitos de Mozarella', 'Dip de Espinacas y Alcachofas', 'Cóctel de Camarones', 'Champiñones Rellenos', 'Tabla de Bruschettas'];
+
+  assignRandomItems(comidasAlPasoElements, comidasAlPaso);
+  assignRandomItems(platosElaboradosElements, platosElaborados);
+  assignRandomItems(cervezasElements, cervezas);
+  assignRandomItems(aperitivosElements, aperitivos);
+
+  for (var i = 0; i < priceElements.length; i++) {
+    var randomPrice = (Math.random() * 1000 + 150).toFixed(2);
+    priceElements[i].textContent = "$" + randomPrice;
+  }
+};
+
+function assignRandomItems(elements, items) {
+  for (var i = 0; i < elements.length; i++) {
+    var randomItem = items[Math.floor(Math.random() * items.length)];
+    elements[i].firstChild.textContent = randomItem;
+  }
+}
